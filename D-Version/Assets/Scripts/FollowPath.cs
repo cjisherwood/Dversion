3b7cdc07592469c086aaa7fbdcde0f8e490f3f40
Nestor Adolfo Macias Montes <Nestor Adolfo Macias Montes>
2017-04-07 11:26:01 -0700

    Class of 04/07, code updated for player functionality

--- D-Version/Assets/Scripts/FollowPath.cs
+++ D-Version/Assets/Scripts/FollowPath.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class FollowPath : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    public Player playerScript;
+
+    private bool[,] origin;
+    private Transform player;
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerScript = gameObject.GetComponent<Player>();
+        player = gameObject.GetComponent<Transform>();
+
+        origin = new bool[playerScript.counter, 5];
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate ()
+    {
+	    
 	}
 }
