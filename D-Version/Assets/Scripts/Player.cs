2f54ef5716cd7d0d698516436bc2650957b44b2c
Liam Connors <liam.r.connors@gmail.com>
2017-04-07 00:13:06 -0700

    Adding main Unity project folder for now.

---
+++ D-Version/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public Transform player;
+
+    void Start () 
+	{
+        player = gameObject.GetComponent<Transform>();
+
+    }
+	
+	void FixedUpdate () 
+	{
+		//Player controls:
+
+		if (Input.GetKey ("w") || Input.GetKey ("up")) 
+		{
+            player.Translate (0, (float) 0.05, 0);
+		}
+		if (Input.GetKey ("s") || Input.GetKey ("down")) 
+		{
+            player.Translate (0, (float) -0.05, 0);
+		}
+		if (Input.GetKey ("d") || Input.GetKey ("right")) 
+		{
+            player.Translate ((float) 0.05, 0, 0);
+		}
+		if (Input.GetKey ("a") || Input.GetKey ("left")) 
+		{
+            player.Translate ((float) -0.05, 0, 0);
+		}
+
+        //Placeholder for resetting level and adding clone.
+        if (Input.GetKeyDown ("r")) 
+		{
+            //Reset player to level start
+            player.position = new Vector3 (0, 3, 0);
+
+            //Add clone
+
+		}
+	}
+}
