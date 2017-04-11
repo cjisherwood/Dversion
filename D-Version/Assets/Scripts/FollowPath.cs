60d3c590f403a90324498bad3b97727b98963cac
Student <Student>
2017-04-11 14:52:08 -0700

    Readding

---
+++ D-Version/Assets/Scripts/FollowPath.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPath : MonoBehaviour {
+
+    public Player playerScript;
+
+    private bool[,] origin;
+    private Transform player;
+
+    // Use this for initialization
+    void Start ()
+    {
+        playerScript = gameObject.GetComponent<Player>();
+        player = gameObject.GetComponent<Transform>();
+
+        origin = new bool[playerScript.counter, 5];
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+	    
+	}
+}
