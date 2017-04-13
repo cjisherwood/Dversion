1d5c3e5fc161d6e8f36ab044d13fb846ebd121dd
Nestor Adolfo Macias Montes <Nestor Adolfo Macias Montes>
2017-04-13 14:00:01 -0700

    Cloning mechanic done, Thank Liam C.

--- D-Version/Assets/Scripts/FollowPath.cs
+++ D-Version/Assets/Scripts/FollowPath.cs
@@ -3,23 +3,50 @@ using System.Collections;
 
 public class FollowPath : MonoBehaviour {
 
-    public Player playerScript;
-
     private bool[,] origin;
     private Transform player;
+	private ulong counter;
+	private float speed;
 
     // Use this for initialization
     void Start ()
     {
-        playerScript = gameObject.GetComponent<Player>();
-        player = gameObject.GetComponent<Transform>();
+		player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+		speed = 0.1f;//FIX
 
-        origin = new bool[playerScript.counter, 5];
+		origin = player.GetComponent<Player> ().originForClone;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
-	    
+		Debug.Log (origin [counter, 0]);
+
+		if (origin[counter, 0])
+		{           
+			gameObject.GetComponent<Transform>().Translate(0, speed, 0);
+		}
+		if (origin[counter, 1])
+		{
+			gameObject.GetComponent<Transform>().Translate(-speed, 0, 0);
+		}
+		if (origin[counter, 2])
+		{
+			gameObject.GetComponent<Transform>().Translate(0, -speed, 0);
+		}
+		if (origin[counter, 3])
+		{
+			gameObject.GetComponent<Transform>().Translate(speed, 0, 0);
+		}
+
+		counter++;
+
+		if (Input.GetKeyDown ("r")) 
+		{
+			//Reset clone to level start
+			gameObject.GetComponent<Transform>().position = new Vector3 (0, 3, 0);
+			counter = 0;
+		}
+			
 	}
 }
--- D-Version/Assets/Scripts/Player.cs
+++ D-Version/Assets/Scripts/Player.cs
@@ -5,17 +5,19 @@ public class Player : MonoBehaviour {
 
     public Transform player;
     public float speed;
-    public uint counter;
+    public ulong counter;
+	public GameObject clone;
 
-    private uint limit;
+    private ulong limit;
     private bool[,] origin;
-    private bool[,] originForClone;
+    public bool[,] originForClone;
 
     void Start () 
 	{
-        player = gameObject.GetComponent<Transform>();
+		player = gameObject.GetComponent<Transform>();
 
-        limit = 3600;
+		limit = 3600;
+		speed = 0.1f;
         origin = new bool[limit, 5];
     }
 	
@@ -24,52 +26,56 @@ public class Player : MonoBehaviour {
         //Player controls
         if (Input.GetKey("w") || Input.GetKey("up"))
         {
-            origin[counter, 1] = true;               
-            player.Translate(0, speed, 0);
+			origin[counter, 0] = true;               
+			player.Translate(0, speed, 0);
         }
         if (Input.GetKey("a") || Input.GetKey("left"))
         {
-            origin[counter, 2] = true;
-            player.Translate(-speed, 0, 0);
+            origin[counter, 1] = true;
+			player.Translate(-speed, 0, 0);
         }
         if (Input.GetKey("s") || Input.GetKey("down"))
         {
-            origin[counter, 3] = true;
-            player.Translate(0, -speed, 0);
+            origin[counter, 2] = true;
+			player.Translate(0, -speed, 0);
         }
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
-            origin[counter, 4] = true;
-            player.Translate(speed, 0, 0);
+            origin[counter, 3] = true;
+			player.Translate(speed, 0, 0);
         }
 
         counter++;
 
-        if (counter > limit)
+        if (counter + 1 > limit)
         {
-            limit += 3600;
+            limit += 3600; //Add a minute
             //need to expand array to origin[limit, 5];
         }
 
         //Placeholder for resetting level and adding clone.
         if (Input.GetKeyDown ("r")) 
 		{
-            //Reset player to level start
-            player.position = new Vector3 (0, 3, 0);
-            counter = 0;
-
             originForClone = origin;
-            origin = new bool[3600, 5];
+            origin = new bool[limit, 5];
 
             //Add clone
-
+			Instantiate (clone, (Vector3.up * 3), Quaternion.identity);
             
-
+			//Reset player to level start
+			player.position = new Vector3 (0, 3, 0);
+			counter = 0;
+			limit = 3600;
         }
 	}
 
-    void CopyArray()
-    {
+	public ulong GetCounter()
+	{
+		return counter;
+	}
 
+    private void CopyArray()
+    {
+        //stuff
     }
 }
