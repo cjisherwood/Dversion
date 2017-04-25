81419449f359463282e0585018687d9c7176b469
Liam Connors <liam.r.connors@gmail.com>
2017-04-24 22:35:52 -0700

    Camera mechanics, applied some sprites temporarily, debug scene added. Cloning still broken, might need to talk about it as a group.

--- D-Version/Assets/Scripts/FollowPath.cs
+++ D-Version/Assets/Scripts/FollowPath.cs
@@ -3,25 +3,29 @@ using System.Collections;
 
 public class FollowPath : MonoBehaviour {
 
+    public int cloneNum;
     private bool[,] origin;
-    private Transform player;
+    private Player player;
 	private ulong counter;
 	private float speed;
+    private string input;
+    private GameObject[] clones;
 
     // Use this for initialization
     void Start ()
     {
-		player = GameObject.FindWithTag("Player").GetComponent<Transform>();
-		speed = 0.1f;//FIX
+        Debug.Log("Checking clone num.");
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+		speed = 0.05f;//FIX
+        clones = GameObject.FindGameObjectsWithTag("Clone");
 
 		origin = player.GetComponent<Player>().originForClone;
+        cloneNum = player.GetComponent<Player>().numOfClones; //player.GetComponent<Player>().CalcNextCloneNum();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
-		Debug.Log (origin [counter, 0]);
-
 		if (origin[counter, 0])
 		{           
 			gameObject.GetComponent<Transform>().Translate(0, speed, 0);
@@ -41,11 +45,69 @@ public class FollowPath : MonoBehaviour {
 
 		counter++;
 
-		if (Input.GetKeyDown ("r")) 
-		{
-			//Reset clone to level start
-			gameObject.GetComponent<Transform>().position = new Vector3 (0, 3, 0);
-			counter = 0;
-		}	
-	}
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            //Reset clone to level start
+            gameObject.GetComponent<Transform>().position = new Vector3(0, 3, 0);
+            counter = 0;
+        }
+
+        //If you hit the number keys, destroy old clones and remap.
+        if (Input.GetKeyDown("1"))
+        {
+            Numbers(1);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            Numbers(2);
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            Numbers(3);
+        }
+        if (Input.GetKeyDown("4"))
+        {
+            Numbers(4);
+        }
+        if (Input.GetKeyDown("5"))
+        {
+            Numbers(5);
+        }
+        if (Input.GetKeyDown("6"))
+        {
+            Numbers(6);
+        }
+        if (Input.GetKeyDown("7"))
+        {
+            Numbers(7);
+        }
+        if (Input.GetKeyDown("8"))
+        {
+            Numbers(8);
+        }
+        if (Input.GetKeyDown("9"))
+        {
+            Numbers(9);
+        }
+    }
+
+    private void Numbers(int num)
+    {
+        if (cloneNum == num)
+        {
+            foreach (GameObject clone in clones)
+            {
+                //Reset clone to level start
+                clone.GetComponent<Transform>().position = new Vector3(0, 3, 0);
+                counter = 0;
+            }
+            player.ResetLevel();
+            GameObject.Destroy(gameObject, 0);
+            player.DestroyClone();
+        }
+    }
 }
--- D-Version/Assets/Scripts/Player.cs
+++ D-Version/Assets/Scripts/Player.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-    
+    public Transform camera;
+
     public float speed;
     public ulong counter;
 	public GameObject clone;
 
     private Transform player;
+    public int numOfClones;
+    public int cloneLimit;
     private ulong limit;
     private bool[,] origin;
     public bool[,] originForClone;
@@ -18,32 +21,47 @@ public class Player : MonoBehaviour {
 		player = gameObject.GetComponent<Transform>();
 
 		limit = 3600;
-		speed = 0.1f;
         origin = new bool[limit, 5];
     }
-	
-	void FixedUpdate () 
-	{
+
+    void FixedUpdate()
+    {
         //Player controls
         if (Input.GetKey("w") || Input.GetKey("up"))
         {
-			origin[counter, 0] = true;               
-			player.Translate(0, speed, 0);
+            origin[counter, 0] = true;
+            player.Translate(0, speed, 0);
+            if(player.position.y - camera.position.y > 2)
+            {
+                camera.Translate(0, speed, 0);
+            }
         }
         if (Input.GetKey("a") || Input.GetKey("left"))
         {
             origin[counter, 1] = true;
-			player.Translate(-speed, 0, 0);
+            player.Translate(-speed, 0, 0);
+            if (player.position.x - camera.position.x < -3.5)
+            {
+                camera.Translate(-speed, 0, 0);
+            }
         }
         if (Input.GetKey("s") || Input.GetKey("down"))
         {
             origin[counter, 2] = true;
-			player.Translate(0, -speed, 0);
+            player.Translate(0, -speed, 0);
+            if (player.position.y - camera.position.y < -2)
+            {
+                camera.Translate(0, -speed, 0);
+            }
         }
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
             origin[counter, 3] = true;
-			player.Translate(speed, 0, 0);
+            player.Translate(speed, 0, 0);
+            if (player.position.x - camera.position.x > 3.5)
+            {
+                camera.Translate(speed, 0, 0);
+            }
         }
 
         counter++;
@@ -54,24 +72,67 @@ public class Player : MonoBehaviour {
             //need to expand array to origin[limit, 5];
         }
 
+    }
+
+    private void Update()
+    {
         //Placeholder for resetting level and adding clone.
-        if (Input.GetKeyDown ("r")) 
-		{
-            originForClone = origin;
-            origin = new bool[limit, 5];
-
-            //Add clone
-			Instantiate (clone, (Vector3.up * 3), Quaternion.identity);
-            
-			//Reset player to level start
-			player.position = new Vector3 (0, 3, 0);
-			counter = 0;
-			limit = 3600;
+        if (Input.GetKeyDown("r"))
+        {
+            CreateClone();
         }
-	}
+    }
 
-	public ulong GetCounter()
+    public ulong GetCounter()
 	{
 		return counter;
 	}
+
+    public void DestroyClone()
+    {
+        numOfClones--;
+    }
+
+    public void ResetLevel()
+    {
+        //Reset player to level start
+        player.position = new Vector3(0, 3, 0);
+        counter = 0;
+        limit = 3600;
+        originForClone = origin;
+        origin = new bool[limit, 5];
+    }
+
+    public void CreateClone()
+    {
+        ResetLevel();
+
+        if (numOfClones < cloneLimit)
+        {
+            //Add clone
+            Instantiate(clone, (Vector3.up * 3), Quaternion.identity);
+            numOfClones++;
+        }
+        else
+        {
+            Debug.Log("Clone limit reached. Clone not created.");
+        }
+    }
+
+    public int CalcNextCloneNum()
+    {
+        GameObject[] clones;
+        int nextClone = 10;
+
+        clones = GameObject.FindGameObjectsWithTag("Clone");
+
+        foreach(GameObject clone in clones)
+        {
+            if (nextClone > clone.GetComponent<FollowPath>().cloneNum && (clone.GetComponent<FollowPath>().cloneNum + 1) != nextClone)
+                nextClone = clone.GetComponent<FollowPath>().cloneNum + 1;
+        }
+
+        Debug.Log(nextClone);
+        return nextClone;
+    }
 }
