d8e8e2ee60119f94d40e83b99094c123fa24f0b3
Nestor Adolfo Macias Montes <namm1994@live.com>
2017-05-14 01:33:01 -0700

    Added some more button sprites

---
+++ D-Version/Assets/Scripts/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void LoadScreen(string sceneNum)
+    {
+        SceneManager.LoadScene(sceneNum);
+    }
+
+    public void LoadOptions()
+    {
+
+    }
+
+    public void LoadExit()
+    {
+
+    }
+}
