c6e5a638fc4f12732b409bf90eb3ea21b906d482
Nestor Adolfo Macias Montes <namm1994@live.com>
2017-05-16 02:00:00 -0700

    Adding updated game main screen

--- D-Version/Assets/Scripts/MenuController.cs
+++ D-Version/Assets/Scripts/MenuController.cs
@@ -1,22 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    public void LoadScreen(string sceneNum)
+    public GameObject ExitMenu;
+    public GameObject OptionsMenu;
+
+    public Button PlayButton;
+    public Button OptionsButton;
+    public Button ExitButton;
+
+    void Start()
+    {
+        ExitMenu.SetActive(false);
+        OptionsMenu.SetActive(false);
+    }
+
+    public void LoadScene(string sceneNum)
     {
         SceneManager.LoadScene(sceneNum);
     }
 
+    #region Options Methods
     public void LoadOptions()
     {
+        PlayButton.enabled = false;
+        ExitButton.enabled = false;
+        OptionsButton.enabled = false;
 
+        OptionsMenu.SetActive(true);
     }
+    public void FullScreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+    #endregion Options Methods
 
+    #region Exit Methods
     public void LoadExit()
     {
+        PlayButton.enabled = false;
+        ExitButton.enabled = false;
+        OptionsButton.enabled = false;
 
+        ExitMenu.SetActive(true);
     }
+
+    public void ExitYesButton()
+    {
+        Debug.Log("Should quit if in application mode. Will not quit in Unity editor.");
+
+        Application.Quit();
+    }
+
+    public void ExitNoButton()
+    {
+        PlayButton.enabled = true;
+        ExitButton.enabled = true;
+        OptionsButton.enabled = true;
+
+        ExitMenu.SetActive(false);
+    }
+    #endregion Exit Methods
 }
