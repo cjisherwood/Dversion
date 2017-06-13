5d819d7dca481d00d632bf17af77a423e96d6714
Nestoring1 <namm1994@live.com>
2017-06-13 10:42:37 -0700

    Adding some more stuff and bug fixes

--- D-Version/Assets/Scripts/LevelExit.cs
+++ D-Version/Assets/Scripts/LevelExit.cs
@@ -6,27 +6,53 @@ using UnityEngine.SceneManagement;
 public class LevelExit : MonoBehaviour
 {
     private bool levelComplete;
+    private Collider2D exitCol;
+    Animator elevatorAnim;
 
 	// Use this for initialization
 	void Start ()
     {
         levelComplete = false;
+        elevatorAnim = gameObject.GetComponent<Animator>();
+        exitCol = GetComponentInChildren<Collider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (levelComplete /*&& animation of elevator has finished*/)
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
+  
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
-            levelComplete = true;
+            ElevatorAnim("Open");
+            StartCoroutine(StartTimer());
+        }
+    }
+
+    public void ElevatorAnim(string state)
+    {
+        elevatorAnim.SetTrigger(state);
+    }
+
+    public IEnumerator StartTimer()
+    {
+        for (int i = 0; i <= 5; i++)
+        {
+            Debug.Log("Starting timer at " + i);
+            yield return new WaitForSecondsRealtime(1);
+
+            if (i == 3)
+            {
+                ElevatorAnim("Close");
+                levelComplete = true;
+            }
+            if (i == 5)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
         }
     }
 }
